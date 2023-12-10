fc55a1314abf34ecd67739f31f4e0498170066a8
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-12-10 19:01:57 +0300

    10.12

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-
-
-def main_func():
-
-    def run_calorizator():
-        calorizator = Calorizator()
-        calorizator.get_products()
-
-    def run_endpoints():
-        pass
-
-
+from Endpoints.endpoints import WellnessEndpoints
 
 if __name__ == "__main__":
-    class Endpoints:
-        def __init__(self):
-            self.db_request = None
-
-        async def main_endpoints(self):
-            app = Flask(__name__)
-            CORS(app)
+    endp = WellnessEndpoints()
+    endp.main_handlers()

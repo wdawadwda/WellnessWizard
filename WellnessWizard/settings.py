c9693091760bd99e9df66366fd6500e3c93a39c3
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-12-11 16:02:25 +0300

    11.12.23

--- WellnessWizard/settings.py
+++ WellnessWizard/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'WellnessWizard_backend',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SITE_URL = '/site/'
 SITE_ROOT = os.path.join(BASE_DIR, 'site')
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:19006",
+]
+

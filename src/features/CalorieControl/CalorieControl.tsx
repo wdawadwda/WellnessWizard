22fc48f578781fec7306dd808d9c846a979a43b1
Dzianis Bychuk <dzianis.bychuk.dev@gmail.com>
2023-12-24 12:47:59 +0300

    calorie control
    feat: calorieControl

---
+++ src/features/CalorieControl/CalorieControl.tsx
@@ -0,0 +1,11 @@
+import { t } from "i18next";
+import { Text, View } from "react-native";
+import { Theme } from "../../store/theme/theme.type";
+
+export default function CalorieControl({ theme }: { theme: Theme }) {
+  return (
+    <View style={{ marginBottom: 50, marginTop: 25 }}>
+
+    </View>
+  );
+}
